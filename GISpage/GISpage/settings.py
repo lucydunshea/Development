aea19a05993748b503a22aa5ea5141dd3829d2df
Lucy Dunshea <lucydunshea6@gmail.com>
2025-02-18 17:42:13 +1300

    switch to ubuntu OS

--- GISpage/GISpage/settings.py
+++ GISpage/GISpage/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,14 +19,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
+DJANGO_SECRET_KEY = 'atestingkeythatmustbechangedinproduction'
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEYZ
+SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['lucydunshea.co.nz']
+ALLOWED_HOSTS = ['lucydunshea.co.nz', '127.0.0.1']
 
 
 # Application definition
@@ -82,8 +80,12 @@ ASGI_APPLICATION = 'GISpage.asgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgres://user:password@host:port/dbname')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
+}
+
 
 
 # Password validation
